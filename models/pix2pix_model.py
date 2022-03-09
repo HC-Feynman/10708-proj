b6d314954217c65713b3f1e9732518fd5e33aa22
huichen <hui.chen96@outlook.com>
2022-03-09 12:51:33 -0500

    add G regularization to make images simpler

--- models/pix2pix_model.py
+++ models/pix2pix_model.py
@@ -118,8 +118,15 @@ class Pix2PixModel(BaseModel):
         self.loss_G_GAN = self.criterionGAN(pred_fake, True)
         # Second, G(A) = B
         self.loss_G_L1 = self.criterionL1(self.fake_B, self.real_B) * self.opt.lambda_L1
+
+        self.loss_G_reg = self.fake_B.sum()
+        g_reg_weight = 1e-3
+
         # combine loss and calculate gradients
-        self.loss_G = self.loss_G_GAN + self.loss_G_L1
+        self.loss_G = self.loss_G_GAN + self.loss_G_L1 + g_reg_weight * self.loss_G_reg
+
+
+
         self.loss_G.backward()
 
     def optimize_parameters(self):
