62f0adebe7639866b22ebe4412268a9e9badf621
huichen <hui.chen96@outlook.com>
2022-03-09 19:28:59 -0500

    change parameter of reg weight

--- models/pix2pix_model.py
+++ models/pix2pix_model.py
@@ -120,7 +120,7 @@ class Pix2PixModel(BaseModel):
         self.loss_G_L1 = self.criterionL1(self.fake_B, self.real_B) * self.opt.lambda_L1
 
         self.loss_G_reg = self.fake_B.sum()
-        g_reg_weight = 1e-3
+        g_reg_weight = 1e-6
 
         # combine loss and calculate gradients
         self.loss_G = self.loss_G_GAN + self.loss_G_L1 + g_reg_weight * self.loss_G_reg
