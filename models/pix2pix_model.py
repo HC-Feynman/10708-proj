b8e97a76dbbce85e14611c834f5510074d9d8318
huichen <hui.chen96@outlook.com>
2022-03-19 08:43:11 -0400

    add semi-sup loss prining

--- models/pix2pix_model.py
+++ models/pix2pix_model.py
@@ -49,6 +49,8 @@ class Pix2PixModel(BaseModel):
         BaseModel.__init__(self, opt)
         # specify the training losses you want to print out. The training/test scripts will call <BaseModel.get_current_losses>
         self.loss_names = ['G_GAN', 'G_L1', 'D_real', 'D_fake']
+        if opt.semi_sup:
+            self.loss_names.append("semi_sup")
         # specify the images you want to save/display. The training/test scripts will call <BaseModel.get_current_visuals>
         self.visual_names = ['real_A', 'fake_B', 'real_B']
         # specify the models you want to save to the disk. The training/test scripts will call <BaseModel.save_networks> and <BaseModel.load_networks>
